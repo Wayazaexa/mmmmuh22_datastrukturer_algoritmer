32022f7fcf0bc17afbeeb0631f5d3ef4524e555c
Wayazaexa <joakimenglund@protonmail.com>
2023-02-23 15:50:31 +0100

    Made a solution to the base assignment in python.

---
+++ Assignments/Assignment 2/python/filecrawler.py
@@ -0,0 +1,33 @@
+import os
+
+def filecrawler(searchString, searchPath):
+    currentPath = None
+    try:
+        for filename in os.listdir(searchPath):
+            currentPath = os.path.join(searchPath, filename)
+
+            if os.path.isdir(currentPath):
+                filecrawler(searchString, currentPath)
+            elif os.path.isfile(currentPath):
+
+                try:
+                    with open(currentPath, 'r', encoding='utf-8') as file:
+                        if searchString in file.read():
+                            print(currentPath)
+                except Exception as fileError:
+                    print(f"Error reading file: {currentPath}; {fileError}")
+    except Exception as folderError:
+        print(f"Error reading folder: {searchPath}; {folderError}")
+
+if __name__ == '__main__':
+
+    # Set path
+    path = os.path.join(os.getcwd(), '../TestData')
+    # Normalize path (removes the unnecessary 'python/..' from the path)
+    search_path = os.path.normpath(path)
+
+    inputString = "y"
+    while (inputString == "yes" or inputString == "y"):
+        inputString = input("What do you want to search for? ")
+        filecrawler(inputString, search_path)
+        inputString = input("Do you want to search again (yes/y)? ")
